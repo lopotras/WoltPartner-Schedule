fb2ab80fce667aea8c57f299edb9c684c3813628
lopotras <14.jedrek@gmail.com>
2020-10-22 15:50:13 +0200

    Added weekday check, refresh verification. Updated function names. Cleanup

--- woltPartnerThursday.py
+++ woltPartnerThursday.py
@@ -1,6 +1,7 @@
 #! python3
 
 from time import sleep as sleep
+from time import strftime as strftime
 
 import win32gui
 
@@ -8,14 +9,9 @@ from pyautogui import click as click
 from pyautogui import typewrite as typewrite
 from pyautogui import hotkey as hotkey
 
-# ATTENTION !!!
-# It is assumed that running this program you:
-#   1.  Have your WoltPartnerApp opened in BlueStacks 4 and auto-aligned.
-#   2.  Have the WoltPartnerApp control mapping imported into Blue BlueStacks.
-#   3.  Are logged into the WoltPartnerApp with your number and are on the
-#       'Scheduled hours -> Aarhus Central' tab.
-
 ### FUNCTIONS DEFINITIONS ###
+timeStamp = strftime("%c")
+
 def wait():
     input("press ENTER to continue...")
 
@@ -34,11 +30,18 @@ def welcome():
     """)
     sleep(1)
 
-def enterWolt():
-    # enters the BlueStack window aligned to the top-left corner of the main screen
-    click(10,70)
+def weekDay():
+    # Verify the weekday
+    day = strftime("%a")
+    if (day == "Thu"):
+        return day
+    elif (day == "Fri"):
+        return day
+    else:
+        print("There are no new shifts coming up today.")
+        return false
 
-def blueStacksSetup():
+def enterWolt():
     hwnd = win32gui.FindWindow(None, "BlueStacks")
     win32gui.ShowWindow(hwnd,5)
     win32gui.SetForegroundWindow(hwnd)
@@ -54,6 +57,7 @@ def changeDay(day):
     # moves to a day that is currently at 'day' position
     # note: 'day' must be an number between 1-7 (position from left to right)
     typewrite(str(day))
+    print(timeStamp + " changed day")
 
 def book(slot):
     # presses the booking button on the chosen slot
@@ -66,6 +70,8 @@ def closeCancel():
     click(282,623)
 
 def get_pixel_colour(i_x, i_y):
+    # returns the color of a pixel with coordinates i_x, i_y
+    # in the format (r, g, b)
     i_desktop_window_id = win32gui.GetDesktopWindow()
     i_desktop_window_dc = win32gui.GetWindowDC(i_desktop_window_id)
     long_colour = win32gui.GetPixel(i_desktop_window_dc, i_x, i_y)
@@ -76,14 +82,16 @@ def pCheck(pc_x1,pc_x2,pc_y):
     for i in range(pc_x1,pc_x2):
         print(get_pixel_colour(i,pc_y))
 
-def verifyPixel(vp_x,vp_y,vp_color):
-    # compares pixel color to 'color'
-    # x and y are pixel coordinates
-    # color options:
-    # w - ("255", 255, 255) - white, background
-    # d - ("81", 81, 84) - black hours text, 1st pixel on the left
-    # b - ("0", 157, 224) - blue, square on day's number
-    # g - ("185", 186, 187) - grey hours text
+def verifyPixelColorChange(vp_x,vp_y,vp_color):
+    # stops until the color changes from vp_color
+    # compares pixel color to the first number of 'color'
+    # vp_x and vp_y are pixel coordinates
+    # vp_color values (in ""):
+    # ("255", 255, 255) - white, background
+    # ("81", 81, 84) - black, active hours text, 1st pixel on the left
+    # ("0", 157, 224) - blue, square on currently chosen day's number
+    # ("185", 186, 187) - grey, inactive hours text
+    # ("250", 250, 250) - grey, refresh circle
     a = get_pixel_colour(vp_x,vp_y)
     while a[0] == vp_color:
         a = get_pixel_colour(vp_x,vp_y)
@@ -98,12 +106,18 @@ def verifyNewHours(vp_x1,vp_x2,vp_y,vp_color):
             if b[0] != vp_color:
                 a += 1
 
-def windowEnumerationHandler(hwnd, top_windows):
-    top_windows.append((hwnd, win32gui.GetWindowText(hwnd)));
+def verifyRefresh():
+    verifyPixelColorChange(273,271,255)
+    print(timeStamp + " refreshing...")
+    verifyPixelColorChange(273,271,250)
+    print(timeStamp + " refreshed")
 
 ### BODY ###
 wait()
 
-blueStacksSetup()
+enterWolt()
+refresh()
+verifyRefresh()
+changeDay(7)
 
 wait();
