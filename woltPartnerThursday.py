481a5d2ca9acf5336e52a80319aee3a9fc426ee5
lopotras <14.jedrek@gmail.com>
2020-10-26 09:48:30 +0100

    Updated mapping
    Switched to in-script mapping in functions: refresh(), changeDay(), bookShift()[name updated]. Added global variables definition space and coordinates lists

--- woltPartnerThursday.py
+++ woltPartnerThursday.py
@@ -1,5 +1,6 @@
 #! python3
 
+### LIBRARIES IMPORT ###
 from time import sleep as sleep
 from time import strftime as strftime
 
@@ -10,9 +11,9 @@ from pyautogui import click as click
 from pyautogui import typewrite as typewrite
 from pyautogui import hotkey as hotkey
 
-### FUNCTIONS DEFINITIONS ###
-timeStamp = strftime("%c")
 
+
+### FUNCTIONS DEFINITIONS ###
 def wait():
     input("press ENTER to continue...")
 
@@ -51,24 +52,19 @@ def enterWolt():
     return rect
 
 def refresh():
-    pyautogui.moveTo(273,271)
+    pyautogui.moveTo( translatedCoordinates(refreshCircle) )
     pyautogui.drag(0,200,0.2,button = 'left')
 
 def changeDay(day):
     # moves to a day that is currently at 'day' position
     # note: 'day' must be an number between 1-7 (position from left to right)
-    typewrite(str(day))
-    print(timeStamp + " changed day")
+    click( translatedCoordinates( thursdayDays[day-1] ) )
+    print( timeStamp + " changed day" )
 
-def book(slot):
-    # presses the booking button on the chosen slot
+def bookShift(slot):
+    # Clicks on the booking button space on the chosen slot
     # note that 'slot' must be a number between 1-14 (position from top to bottom)
-    position = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'q', 'w', 'e', 'r']
-    hotkey('shift', position[slot-1])
-
-def closeCancel():
-    # closes the booking cancel window without canceling
-    click(282,623)
+    click( translatedCoordinates( shifts[slot-1] ) )
 
 def get_pixel_colour(i_x, i_y):
     # returns the color of a pixel with coordinates i_x, i_y
@@ -113,6 +109,50 @@ def verifyRefresh():
     verifyPixelColorChange(273,271,250)
     print(timeStamp + " refreshed")
 
+def translatedCoordinates(xyIn):
+    ### Translator for the coordinates in 0-1 value range ###
+    # Translates the relative coordinates into pixel position on the screen
+    # using the current window position and size
+    hwnd = win32gui.FindWindow(None, "BlueStacks")
+    rect = win32gui.GetWindowRect(hwnd)
+    xOut = rect[0] + ( xyIn[0] * (rect[2]-rect[0]) )
+    yOut = rect[1] + ( xyIn[1] * (rect[3]-rect[1]) )
+    return [xOut, yOut]
+
+
+
+### GLOBAL VARIABLES DEFINITIONS ###
+timeStamp = strftime("%c")
+
+# RELATIVE COORDINATES
+
+refreshCircle = [0.4370015948963317, 0.26601941747572816]
+
+shifts = [[0.8229665071770335, 0.27475728155339807],
+            [0.810207336523126, 0.3300970873786408],
+            [0.8118022328548644, 0.38058252427184464],
+            [0.8118022328548644, 0.4349514563106796],
+            [0.8133971291866029, 0.49029126213592233],
+            [0.8149920255183413, 0.5446601941747573],
+            [0.8181818181818182, 0.5980582524271845],
+            [0.8086124401913876, 0.6524271844660194],
+            [0.8133971291866029, 0.7029126213592233],
+            [0.8070175438596491, 0.7621359223300971],
+            [0.8070175438596491, 0.8097087378640777],
+            [0.8054226475279107, 0.8699029126213592],
+            [0.8133971291866029, 0.9262135922330097],
+            [0.8054226475279107, 0.9757281553398058]]
+
+thursdayDays = [[0.05422647527910686, 0.2058252427184466],
+                [0.1419457735247209, 0.2058252427184466],
+                [0.22169059011164274, 0.2058252427184466],
+                [0.3014354066985646, 0.20388349514563106],
+                [0.379585326953748, 0.20388349514563106],
+                [0.46411483253588515, 0.20485436893203884],
+                [0.5454545454545454, 0.20194174757281552]]
+
+
+
 ### BODY ###
 wait()
 
@@ -120,5 +160,6 @@ enterWolt()
 refresh()
 verifyRefresh()
 changeDay(7)
+bookShift(9)
 
 wait();
