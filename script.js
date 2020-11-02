0785bd3ca93fa2fd74a872db43cf13df8cd9cff2
lopotras <14.jedrek@gmail.com>
2020-11-02 12:13:37 +0100

    Added copy button functionality
    Updated code generation

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// FUNCTIONS //
+
 // Removes a value from an array
 function arrayRemove( arr, value ) {
 
@@ -6,50 +8,25 @@ function arrayRemove( arr, value ) {
   }  );
 }
 
-// define array for checked bookings (checkbox)
-var bookings = [];
-
-
-// define string for ready python code
-var code = "";
-
-
-// define object that matches the day with the corresponding position in WoltPartner App
-const days = {
-  "Mon": 4,
-  "Tue": 5,
-  "Wed": 6,
-  "Thu": 7,
-  "Fri": 5,
-  "Sat": 6,
-  "Sun": 7,
-};
-
-
-// define regular expression to match checkbox ID's from 'bookings'
-const re = /([A-Za-z]+)(\d)/;
-
-
-// define function translating checkboxIDs array into Python code
-// takes in array, returns a string
+/* Define function translating checkboxIDs array into Python code
+  takes in array, returns a string */
 function toPython(arr) {
-  let str = "\n", add, arg1, arg2;
+  let str = "", add, arg1, arg2;
 
   for (let i = 0; i < arr.length; i++) {
 
-    arg1 = days[  arr[i].match( re ) [1]  ];
-    arg2 = arr[i].match( re ) [2];
+    arg1 = days[ arr[i].match(re)[1] ];
+    arg2 = arr[i].match(re) [2];
 
-    add = `booking(${arg1},${arg2})\n`;
+    add = `\tbooking(${arg1},${arg2})\n`;
     str = str.concat( add );
   };
 
   return str;
 }
 
-
-// define function that filters through 'bookings' and returns array filled
-// only with day that need to be booked on Thursday
+/* Define function that filters through 'bookings' and returns array filled
+  only with day that need to be booked on Thursday */
 function thuDays(arr) {
 
   return arr.filter( function(ele) {
@@ -62,9 +39,8 @@ function thuDays(arr) {
   });
 }
 
-
-// define function that filters through 'bookings' and returns array filled
-// only with day that need to be booked on Friday
+/* Define function that filters through 'bookings' and returns array filled
+  only with day that need to be booked on Friday */
 function friDays(arr) {
 
   return arr.filter( function(ele) {
@@ -77,14 +53,65 @@ function friDays(arr) {
   });
 }
 
+// Define copying function
+const copyToClipboard = str => {
+  const el = document.createElement('textarea');
+  el.value = str;
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+};
+
+//Define copy function for Thursday code
+const copyThu = () => {
+  copyToClipboard(document.getElementById("thuCode").innerHTML)
+}
+
+//Define copy function for Friday code
+const copyFri = () => {
+  copyToClipboard(document.getElementById("friCode").innerHTML)
+}
+
+
+
+// VARIABLES //
+
+// Define array for checked bookings (checkbox)
+var bookings = [];
+
+// Define string for ready python code
+var code = "";
+
+
+// Define object that matches the day with the corresponding position in WoltPartner App
+const days = {
+  "Mon": 4,
+  "Tue": 5,
+  "Wed": 6,
+  "Thu": 7,
+  "Fri": 5,
+  "Sat": 6,
+  "Sun": 7,
+};
+
+
+// Define regular expression to match checkbox ID's from 'bookings'
+const re = /([A-Za-z]+)(\d)/;
+
+const refresh = "\n\n\trefresh()\n\n"
+
 
 // BODY //
 
 // Putting a checkbox-table into code
 $(document).ready( function(){
 
-  // Add an element to 'bookings' when a checkbox is checked
-  // Remove an element from 'bookings' when a checkbox is unchecked
+  /* Add an element to 'bookings' when a checkbox is checked
+   Remove an element from 'bookings' when a checkbox is unchecked */
   $( ":checkbox" ).click( function() {
 
     if (  bookings.includes( this.id )  ) {
@@ -93,8 +120,8 @@ $(document).ready( function(){
     } else { bookings.push( this.id ); };
 
     //show Python code in a respective space according to the day of booking
-    $( "#thuCode" ).html(toPython( thuDays(bookings) ));
-    $( "#friCode" ).html(toPython( friDays(bookings) ));
+    $( "#thuCode" ).html("\tstartAt(15)" + refresh + toPython( thuDays(bookings) ));
+    $( "#friCode" ).html("\tstartAt(8)" + refresh + toPython( friDays(bookings) ));
 
   });
 
